6e541ad6f45997ea03dd900d83e32cb1cb38cf29
yuriMartinst <105825445+yuriMartinst@users.noreply.github.com>
2022-06-04 21:03:21 -0300

    Add files via upload
    Não consegui desenvolver o codigo com exito

---
+++ TrabalhoLean.java
@@ -0,0 +1,22 @@
+R1- Variável As variáveis ​​podem ser encontradas em vários valores. Os elementos são referenciados por índice e o número inserido entre eles
+Uma coleção de referências ao conteúdo da variável. Arrays podem ser unidimensionais (unidimensional - vetor) e bidimensionais (bidimensional - matriz).
+
+R2- Matrizes são arranjos em 2 dimenções, por isso eles possuem dois índices e por isso permitem que tenha valores armazenados na forma de matriz 
+
+R3-
+classe Main {
+	public static void main(String[] args){
+int v1[]={2,4,7,13,14,15,16};
+int v2[]={1,6,7,11,13,16,18};
+    int c []="uniao"(v1,v2;)
+    system.out.printl(arrays.ToString(c));
+
+    public static int uniao[](v1[], v2[]){
+      int c[]="novo" int[v1.comprimento+v2.comprimento];
+    intk=0;
+    
+    for(int i=0;i<v1.length;i++){
+      c[k++]=v1[i];
+
+      for(int i=0;i<v2.length;i++){
+      c[k++]=v2[i];
